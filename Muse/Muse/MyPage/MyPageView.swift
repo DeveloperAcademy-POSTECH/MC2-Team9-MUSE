100d215b202348a5ca79b2acc963bc04e414e2c7
taehyeonk <xogus9641@naver.com>
2022-06-11 03:39:32 +0900

    feat: 헤더 추가 및 Segment로 list 출력 구현

--- Muse/Muse/MyPage/MyPageView.swift
+++ Muse/Muse/MyPage/MyPageView.swift
@@ -7,9 +7,57 @@
 
 import SwiftUI
 
-struct MyPageView: View {
+enum LibraryType: String, CaseIterable {
+    case myTicket = "나의 음악 티켓"
+    case saved = "저장한 티켓"
+}
+
+struct TicketListView: View {
+    var ticketList: String
+    
     var body: some View {
-        Text("내 라이브러리 뷰!")
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {_ in
+                    Text(ticketList)
+                }
+            }
+        }
+    }
+}
+
+struct ChosenView: View {
+    var selectedSide: LibraryType
+    
+    var body: some View {
+        switch selectedSide {
+            case .myTicket:
+                TicketListView(ticketList: "myTicketList")
+            case .saved:
+                TicketListView(ticketList: "savedTicketList")
+        }
+    }
+}
+
+struct MyPageView: View {
+    @State private var selectedSide: LibraryType = .myTicket
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Picker("Choose a Side", selection: $selectedSide) {
+                    ForEach(LibraryType.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                ChosenView(selectedSide: selectedSide)
+            }
+            .navigationTitle("내 라이브러리")
+            .background(Color(red: 248/255, green: 248/255, blue: 248/255).edgesIgnoringSafeArea(.all))
+        }
     }
 }
 
