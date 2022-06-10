24430c45db49412e83a05f93ea06df13f6d8118e
taehyeonk <xogus9641@naver.com>
2022-06-10 10:41:38 +0900

    create project

---
+++ Muse/Muse/MuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseApp.swift
+//  Muse
+//
+//  Created by Admin on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct MuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
