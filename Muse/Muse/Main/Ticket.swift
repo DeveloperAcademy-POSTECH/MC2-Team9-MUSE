ee4ce70d3db8e083d7532d5c61922d6517ff9bdc
Willowwryu <tuyuyou@gmail.com>
2022-06-13 09:42:42 +0900

    TicketView 생성

---
+++ Muse/Muse/Main/Ticket.swift
@@ -0,0 +1,20 @@
+//
+//  Ticket.swift
+//  Muse
+//
+//  Created by 유재훈 on 2022/06/13.
+//
+
+import SwiftUI
+
+struct Ticket: View {
+    var body: some View {
+        Image("ticket")
+    }
+}
+
+struct Ticket_Previews: PreviewProvider {
+    static var previews: some View {
+        Ticket()
+    }
+}
