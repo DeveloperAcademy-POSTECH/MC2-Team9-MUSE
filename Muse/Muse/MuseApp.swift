13053b0ee20d79657a7e9194ae7b86de90e10c3d
Ayden <kaitaro77@naver.com>
2022-06-14 09:47:12 +0900

    feat: 티켓 쓰기 뷰 구성. Asset에 티켓 이미지 변경
    Song Search Bar, Comment 생성 : 타이핑 가능.
    버튼 생성 : but, 작동 코드 x.
    티켓 이미지 변경 : 선 색깔을 검정 -> 회색으로 바꿈.

--- Muse/Muse/MuseApp.swift
+++ Muse/Muse/MuseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MuseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MakeTicketView(searchText: "", comment: "")
         }
     }
 }
